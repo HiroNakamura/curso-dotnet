d11782122334a8e9057eadbe1de94d8d8bccd790
Fer_Carraro <carraro.fernando@gmail.com>
2019-09-28 21:52:47 -0500

    Se ha creado clase Programador.cs

---
+++ temario/chapter3/enumeraciones/Programador.cs
@@ -0,0 +1,10 @@
+namespace enumeraciones
+{
+    public class Programador
+    {
+        public static string JUNIOR = "JUNIOR";
+        public static string MID_LEVEL = "MID_LEVEL";
+        public static string SENIOR = "SENIOR";
+        public static string MASTER = "MASTER";
+    }
+}
