2b08c103bda21136b5294234f7fa69baa19aef30
Fer_Carraro <carraro.fernando@gmail.com>
2019-09-29 11:58:25 -0500

    Se ha editado chapter7: se agrego .gitignore

---
+++ temario/chapter7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace chapter7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
