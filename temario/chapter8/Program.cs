8f02a21f9a4dfcdb575b664d0c89baef0ca4379b
Fer_Carraro <carraro.fernando@gmail.com>
2019-09-30 17:34:09 -0500

    Editado chapter8: se creo archivo datos.csv, MyFile.cs editado

--- temario/chapter8/Program.cs
+++ temario/chapter8/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.IO;
+using clases;
 
 namespace chapter8
 {
     class Program
     {
+        public static readonly string PATH = @"C:\\Users\\DELL\\Documents\\programacion\\testRepositories\\curso-dotnet\\temario\\chapter8\\datos.csv";
         static void Main(string[] args)
         {
             TestMockA();
@@ -12,7 +15,29 @@ namespace chapter8
         static void TestMockA()
         {
             Console.WriteLine("*** File in C# ***");
+            var myFile = new MyFile();
+
+            if(File.Exists(PATH))
+            {
+                myFile.Existe = true;
+            }
+
+            myFile.Nombre = PATH;
+            DateTime fechaActualizacion = File.GetLastAccessTime(PATH);
+            myFile.UltimoAcceso = fechaActualizacion;
+            Console.WriteLine("{0}",myFile);
+
+            Console.WriteLine("Leyendo contenido...");
+            using (StreamReader sr = File.OpenText(myFile.Nombre)){
+                string s;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+
             
+
         }
     }
 }
--- temario/chapter8/clases/MyFile.cs
+++ temario/chapter8/clases/MyFile.cs
@@ -1,15 +1,41 @@
 using System;
-using System.IO;
 
 namespace clases
 {
   public class MyFile
   {
   
-      private string nombre;
-      
-      public MyFile(){}
+      public string Nombre
+      {
+        get;set;
+      }
+
+      public DateTime UltimoAcceso
+      {
+        get;set;
+      }
+
+      public bool Existe
+      {
+        get;set;
+      }
+      public MyFile(){
+        Console.WriteLine("Objeto MyFile creado e inicializado");
+      }
+
+      public MyFile(string nombre, DateTime ultimoAcceso, bool existe):base()
+      {
+        Nombre = nombre;
+        UltimoAcceso = ultimoAcceso;
+        Existe = existe;
+      }
       
       ~MyFile(){}
+
+      public override string ToString()
+      {
+        string result = Existe==true? "existe" : "no existe"; 
+        return String.Format("El archivo {0} {1} , el ultimo acceso fue el {2}",Nombre,result,UltimoAcceso);
+      }
   }
 }
