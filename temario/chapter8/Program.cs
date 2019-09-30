6e3185fafd2d187ddc0c56122872221154ced3dd
Fer_Carraro <carraro.fernando@gmail.com>
2019-09-29 20:45:31 -0500

    Se ha creado chapter8

---
+++ temario/chapter8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace chapter8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestMockA();
+        }
+
+        static void TestMockA()
+        {
+            Console.WriteLine("*** File in C# ***");
+            
+        }
+    }
+}
