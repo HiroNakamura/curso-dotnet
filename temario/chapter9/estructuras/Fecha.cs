729c9760755dd4be3f185d604b0354fd0dc30112
ARIEL <carraro.ariel@gmail.com>
2019-11-10 19:31:19 -0600

    Se ha creado la estructura Fecha.cs

---
+++ temario/chapter9/estructuras/Fecha.cs
@@ -0,0 +1,54 @@
+using System;
+
+namespace estructuras
+{
+    struct Fecha
+    {
+        public Fecha(int anyo, int mes, in dia, int hora,int minutos, int segundos){
+            Mes = mes;
+            Anyo = anyo;
+            Dia = dia;
+            Hora = hora;
+            Minutos = minutos;
+            Segundos = segundos;
+            Console.WriteLine("Estructura Fecha creada");
+        }
+        
+        public int Anyo
+        {
+            get;set;
+        }
+
+        public int Mes
+        {
+            get;set;
+        }
+        
+        public int Dia
+        {
+           get; set;
+        }
+        
+        public int Hora
+        {
+           get; set;
+        }
+        
+        public int Minutos
+        {
+           get; set;
+        }
+        
+        public int Segundos
+        {
+           get; set;
+        }
+        
+      
+        public string GetFecha()
+        {
+            return Anyo + " - " + Mes+" - "+Dia+"-"+Hora+":"+Minutos+":"+Segundos;
+        }
+
+    }
+}
