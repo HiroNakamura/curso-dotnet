2ff6cd909c7f6e9e4588597b8e4f09b9b06f7649
ARIEL <carraro.ariel@gmail.com>
2019-12-03 13:14:01 -0600

    Diccionario.cs creado
    Ejemplo de Diccionario

---
+++ temario/chapter3/diccionarios/Diccionario.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace diccionarios
+{
+   
+   public class Diccionario
+   {
+       public Diccionario()
+       {
+         Console.WriteLine("Objeto Diccionario creado e inicializado");
+       }
+       ~Diccionario(){}
+   }
+
+}
